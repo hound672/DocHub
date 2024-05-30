ade4327f95790287ac6ba3375d249b26bfcf6517
hound672 <hound672@gmail.com>
2024-05-30 12:50:18 +0300

    add volume

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ fi
 rm -rf ./public/documentation/*
 cp -r ${ASOC_ARCH_PATH}/* ./public/documentation/
 mv ./public/documentation/dochub.yaml ./public/documentation/root.yaml
-docker-compose build nginx --no-cache
-docker save dochub:latest -o dochub.img
+scp -r ./public/documentation/root.yaml ./public/documentation/AppSec.iml ./public/documentation/arch ./public/documentation/docs ./public/documentation/entities pt-dochub:DocHub/public/documentation
+#docker-compose build nginx --no-cache
+#docker save dochub:latest -o dochub.img
 
